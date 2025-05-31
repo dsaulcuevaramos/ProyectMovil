fb0e64457084b31c83d3836b35b1ff9f617d3aa2
DiegoCueva <dsaulcuevaramos@gmail.com>
2025-05-30 22:42:53 -0500

    add packages

--- app/src/main/java/com/codedev/proyectmovil/Activities/MainActivity.java
+++ app/src/main/java/com/codedev/proyectmovil/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.codedev.proyectmovil;
+package com.codedev.proyectmovil.Activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.codedev.proyectmovil.R;
 
 public class MainActivity extends AppCompatActivity {
 
