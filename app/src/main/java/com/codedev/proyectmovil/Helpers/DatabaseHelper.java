378324c32537042fe7146b74d8f26687beb34b30
isa-batch <isaipaucardevices@gmail.com>
2025-06-14 18:07:01 -0500

    feat: creación de Tabla y vistas de Usuario

---
+++ app/src/main/java/com/codedev/proyectmovil/Helpers/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.codedev.proyectmovil.Helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.codedev.proyectmovil.Helpers.Usuario.UsuarioTable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Personas.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(UsuarioTable.SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(UsuarioTable.SQL_DROP);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/codedev/proyectmovil/Helpers/Usuario/UsuarioDAO.java
@@ -0,0 +1,75 @@
+package com.codedev.proyectmovil.Helpers.Usuario;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.codedev.proyectmovil.Helpers.DatabaseHelper;
+import com.codedev.proyectmovil.Models.UsuarioModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDAO {
+    private DatabaseHelper helper;
+
+    public UsuarioDAO(Context context) {
+        helper = new DatabaseHelper(context);
+    }
+
+    public boolean addUsuario(UsuarioModel u){
+        try (SQLiteDatabase db = this.helper.getWritableDatabase()){
+            ContentValues values = new ContentValues();
+            values.put(UsuarioTable.COL_NOMBRE, u.getNombre());
+            values.put(UsuarioTable.COL_CORREO, u.getCorreo());
+            values.put(UsuarioTable.COL_CONTRASENIA, u.getContrasenia());
+            values.put(UsuarioTable.COL_ESTADO, 1);
+            long resultado = db.insert(UsuarioTable.TABLE_NAME, null, values);
+            return resultado != -1;
+        }
+    }
+
+    public boolean updateUsuario(UsuarioModel u){
+        try (SQLiteDatabase db = this.helper.getWritableDatabase()){
+            ContentValues values = new ContentValues();
+            values.put(UsuarioTable.COL_NOMBRE, u.getNombre());
+            values.put(UsuarioTable.COL_CORREO, u.getCorreo());
+            values.put(UsuarioTable.COL_CONTRASENIA, u.getContrasenia());
+            values.put(UsuarioTable.COL_ESTADO, u.getEstado());
+            long filas = db.update(UsuarioTable.TABLE_NAME, values, UsuarioTable.COL_ID + " = ?", new String[]{String.valueOf(u.getId())});
+            return filas > 0;
+        }
+    }
+
+    public boolean deleteUsuario(int id){
+        try (SQLiteDatabase db = this.helper.getWritableDatabase()){
+            ContentValues values = new ContentValues();
+            values.put(UsuarioTable.COL_ESTADO, 0);
+            long filas = db.update(UsuarioTable.TABLE_NAME, values, UsuarioTable.COL_ID + " =?", new String[]{String.valueOf(id)});
+            return filas > 0;
+        }
+    }
+
+    public List<UsuarioModel> getUsuarios(){
+        try (SQLiteDatabase db = this.helper.getReadableDatabase()){
+            List<UsuarioModel> listaUsuarios = new ArrayList<>();
+            Cursor cursor = db.rawQuery("SELECT * FROM " + UsuarioTable.TABLE_NAME + " WHERE " + UsuarioTable.COL_ESTADO + " = 1", null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    UsuarioModel u = new UsuarioModel();
+                    u.setId(cursor.getInt(cursor.getColumnIndexOrThrow(UsuarioTable.COL_ID)));
+                    u.setNombre(cursor.getString(cursor.getColumnIndexOrThrow(UsuarioTable.COL_NOMBRE)));
+                    u.setCorreo(cursor.getString(cursor.getColumnIndexOrThrow(UsuarioTable.COL_CORREO)));
+                    u.setContrasenia(cursor.getString(cursor.getColumnIndexOrThrow(UsuarioTable.COL_CONTRASENIA)));
+                    u.setEstado(cursor.getInt(cursor.getColumnIndexOrThrow(UsuarioTable.COL_ESTADO)));
+                    listaUsuarios.add(u);
+                } while (cursor.moveToNext());
+            }
+
+            cursor.close();
+            return listaUsuarios;
+        }
+    }
+}
