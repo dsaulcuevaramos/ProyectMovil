809b31c6d65c0af6fa70ca90533866bc67095073
Sedaasu12 <122957359+Sedaasu12@users.noreply.github.com>
2025-07-02 20:12:20 -0500

    a

--- app/src/main/java/com/codedev/proyectmovil/Helpers/Facultad/FacultadDAO.java
+++ app/src/main/java/com/codedev/proyectmovil/Helpers/Facultad/FacultadDAO.java
@@ -17,8 +17,8 @@ public class FacultadDAO {
         helper = new DatabaseHelper(context);
     }
 
-    public List<FacultadModel> getFacultad(){
-        try (SQLiteDatabase db = this.helper.getReadableDatabase()){
+    public List<FacultadModel> getFacultad() {
+        try (SQLiteDatabase db = this.helper.getReadableDatabase()) {
             List<FacultadModel> listaFacultades = new ArrayList<>();
             Cursor cursor = db.rawQuery("SELECT * FROM " + FacultadTable.TABLE_NAME + " WHERE " + FacultadTable.COL_ESTADO + " = 1", null);
 
@@ -37,4 +37,82 @@ public class FacultadDAO {
             return listaFacultades;
         }
     }
+
+    public boolean addFacultad(FacultadModel facultadModel) {
+        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(FacultadTable.COL_CODIGO, facultadModel.getCodigo());
+            values.put(FacultadTable.COL_NOMBRE, facultadModel.getNombre());
+            values.put(FacultadTable.COL_ESTADO, 1);
+            long resultado = db.insert(FacultadTable.TABLE_NAME, null, values);
+            return resultado != -1;
+        }
+    }
+
+    public boolean updateFacultad(FacultadModel facultadModel) {
+        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(FacultadTable.COL_CODIGO, facultadModel.getCodigo());
+            values.put(FacultadTable.COL_NOMBRE, facultadModel.getNombre());
+            values.put(FacultadTable.COL_ESTADO, facultadModel.getEstado());
+            int filas = db.update(FacultadTable.TABLE_NAME, values,
+                    FacultadTable.COL_ID + " = ?",
+                    new String[]{String.valueOf(facultadModel.getId())});
+            return filas > 0;
+        }
+    }
+
+    public boolean deleteFacultad(int id) {
+        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(FacultadTable.COL_ESTADO, 0);
+            int filas = db.update(FacultadTable.TABLE_NAME, values,
+                    FacultadTable.COL_ID + " = ?",
+                    new String[]{String.valueOf(id)});
+            return filas > 0;
+        }
+    }
+
+    public List<FacultadModel> getFacultades() {
+        List<FacultadModel> listaFacultades = new ArrayList<>();
+        try (SQLiteDatabase db = helper.getReadableDatabase()) {
+            Cursor cursor = db.rawQuery(
+                    "SELECT * FROM " + FacultadTable.TABLE_NAME +
+                            " WHERE " + FacultadTable.COL_ESTADO + " = 1", null);
+
+            if (cursor.moveToFirst()) {
+                do {
+                    FacultadModel facultad = new FacultadModel();
+                    facultad.setId(cursor.getInt(cursor.getColumnIndexOrThrow(FacultadTable.COL_ID)));
+                    facultad.setCodigo(cursor.getString(cursor.getColumnIndexOrThrow(FacultadTable.COL_CODIGO)));
+                    facultad.setNombre(cursor.getString(cursor.getColumnIndexOrThrow(FacultadTable.COL_NOMBRE)));
+                    facultad.setEstado(cursor.getInt(cursor.getColumnIndexOrThrow(FacultadTable.COL_ESTADO)));
+                    listaFacultades.add(facultad);
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
+        return listaFacultades;
+    }
+
+    public FacultadModel getFacultadById(int id) {
+        try (SQLiteDatabase db = helper.getReadableDatabase()) {
+            Cursor cursor = db.query(FacultadTable.TABLE_NAME, null,
+                    FacultadTable.COL_ID + " = ? AND " + FacultadTable.COL_ESTADO + " = 1",
+                    new String[]{String.valueOf(id)}, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                FacultadModel facultad = new FacultadModel();
+                facultad.setId(cursor.getInt(cursor.getColumnIndexOrThrow(FacultadTable.COL_ID)));
+                facultad.setCodigo(cursor.getString(cursor.getColumnIndexOrThrow(FacultadTable.COL_CODIGO)));
+                facultad.setNombre(cursor.getString(cursor.getColumnIndexOrThrow(FacultadTable.COL_NOMBRE)));
+                facultad.setEstado(cursor.getInt(cursor.getColumnIndexOrThrow(FacultadTable.COL_ESTADO)));
+                cursor.close();
+                return facultad;
+            }
+            if (cursor != null) cursor.close();
+        }
+        return null;
+    }
+
 }
